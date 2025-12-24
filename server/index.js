11b0300bea8b1c27264f69fbc92fd6fafb470463
Kartikey2011yadav <nykay10968@gmail.com>
2025-12-24 18:58:03 +0530

    backend deployement correction

--- server/index.js
+++ server/index.js
@@ -39,6 +39,10 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
